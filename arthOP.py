9f0aa46d486c64d3f1cfabeebafd0d641a0b795d
Thushar Gowda <136103368+thushar-gowda@users.noreply.github.com>
2023-06-26 06:47:04 +0530

    arthOP.py

---
+++ arthOP.py
@@ -0,0 +1,12 @@
+n1=int(input("Enter 1st number >>"))
+n2=int(input("Enter 2nd number >>"))
+sum=n1+n2
+diff=n1-n2
+mult=n1*n2
+div=n1/n2
+modu=n1%n2
+print(sum)
+print(diff)
+print(mult)
+print(div)
+print(modu)
