b1de8a9b02975caf060051b212d3c74e52f3db0e
Thushar Gowda <136103368+thushar-gowda@users.noreply.github.com>
2023-06-26 06:48:45 +0530

    eo&pn.py

---
+++ eo&pn.py
@@ -0,0 +1,13 @@
+# Even or Odd
+n=int(input("Enter a number >> "))
+if n%2==0:
+    print("Even")
+else:
+    print("Odd")
+
+# Pos or Neg
+n1=int(input("Enter a number >> "))
+if n1>=0:
+    print("Number is Positive")
+else:
+    print("Number is Negative")
