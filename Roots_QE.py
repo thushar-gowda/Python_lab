9a60b8ee59fdd9abf68004f90f839c73e9f8510c
Thushar Gowda <136103368+thushar-gowda@users.noreply.github.com>
2023-06-26 06:36:03 +0530

    Roots_QE.py

---
+++ Roots_QE.py
@@ -0,0 +1,27 @@
+import math
+
+def find_roots(a, b, c):
+    discriminant = b**2 - 4*a*c
+
+    if discriminant > 0:
+        root1 = (-b + math.sqrt(discriminant)) / (2*a)
+        root2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return root1, root2
+    elif discriminant == 0:
+        root = -b / (2*a)
+        return root
+    else:
+        # Complex roots
+        real_part = -b / (2*a)
+        imaginary_part = math.sqrt(-discriminant) / (2*a)
+        root1 = complex(real_part, imaginary_part)
+        root2 = complex(real_part, -imaginary_part)
+        return root1, root2
+
+# Example usage
+a = 1
+b = -3
+c = 2
+
+roots = find_roots(a, b, c)
+print("Roots:", roots)
